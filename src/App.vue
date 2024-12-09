4e8224a563a582ec7357697723fb6766d1c56bf8
CS Navodya <int.navodya@creativesoftware.com>
2024-12-09 15:41:09 +0530

    Removed vue-class-component

--- src/App.vue
+++ src/App.vue
@@ -3,15 +3,15 @@
 </template>
 
 <script lang="ts">
-import { Options, Vue } from 'vue-class-component';
+import { defineComponent } from 'vue';
 import HomePage from './pages/HomePage.vue';
 
-@Options({
+export default defineComponent({
+  name: 'App',
   components: {
-    HomePage
+    HomePage,
   },
-})
-export default class App extends Vue {}
+});
 </script>
 
 <style>
